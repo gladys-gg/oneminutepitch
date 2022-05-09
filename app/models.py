1af5fb270ab18279cc66dde7934e8207af65a10a
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-09 22:59:51 +0300

    Added the pitch model

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True)
     password = db.Column(db.String(255))
-    # pitches = db.relationship('Pitch', backref='author', lazy="dynamic")
+    pitches = db.relationship('Pitch', backref='author', lazy="dynamic")
     # comments = db.relationship('Comment', backref='user', lazy="dynamic")
 
     def save_user(self):
@@ -17,3 +17,23 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+    
+    
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String(50))
+    content = db.Column(db.String(300))
+    category_id = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+
+
+    def save_pitch(self, pitch):
+        ''' Save the pitches '''
+        db.session.add(pitch)
+        db.session.commit()    
+        
+    def __repr__(self):
+        return f"Pitch('{self.title}')"
