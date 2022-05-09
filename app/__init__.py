4e24915b2d0f1a9e882c7b5cc42748b11a4b4989
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-09 23:51:09 +0300

    implementee bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,15 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-def create_app(config_name):
-
-    app = Flask(__name__)
-    
-    
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
-    # config_options[config_name].init_app(app)
+from flask_bcrypt import Bcrypt
 
 
-    #Initializing the flask extensions
-    db.init_app(app)
-    
-    # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+app = Flask(__name__)
+app.config['SECRET_KEY']='hithere123'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///pitches.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
-    return app
+return app
