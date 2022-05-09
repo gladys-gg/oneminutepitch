7cbb1eef264a534ae09a6855baec1488d5ad7c80
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-09 22:02:51 +0300

    Implemented the application structure

---
+++ config.py
@@ -0,0 +1,35 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Gmwangi@localhost/watchlist'
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
