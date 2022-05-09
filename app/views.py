1728aafc954b3c18a9f29b317e0b02044822b7be
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-09 23:53:00 +0300

    implemented routes for signup and signin

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,redirect,flash,url_for,request
-from app import app,db
+from app import app,db,bcrypt
 from app.models import User, Pitch
 from .forms import *
 
@@ -8,3 +8,35 @@ from .forms import *
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created successfully. You can now login', 'success')
+        return redirect(url_for('signIn'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error in creating the user:(err_msg)')
+    return render_template('signUp.html', form= form)
+
+@app.route('/signIn', methods=['GET', 'POST'])
+def signIn():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form=LogInForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user,remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check your username and password', 'danger')
+    return render_template('signin.html', title = 'Login', form= form)
+
